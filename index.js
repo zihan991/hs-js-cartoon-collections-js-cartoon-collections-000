acefbb8c83ba98c02cfb16ffb8d15bb9ef267206
celestial-applet-1261 <zihan991@gmail.com>
2020-11-11 21:16:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves){ 
+var array = [];
+for (var i = 0;i < dwarves.length;i++)
+  array.push(`${i+1}. ${dwarves[i]} `);
+return array.join("");
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
-}
+var array = [];
+for(let i= 0; i < planeteerCalls.length ; i++){
+  let exclamation = '!'; 
+  array.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase());
+  }
+  return array;
+  }
 
 function longPlaneteerCalls(words) {
-}
+  for(var i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      return true;
+      } 
+      else {
+        return false;
+        }
+  }
+  }
 
 function findTheCheese (foods) {
+  
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] ===  'gouda'
+    || foods[i]===  'cheddar' 
+    || foods[i]==='camembert') {
+    return foods[i];
+    }
+    
+  }return 'no cheese!';
 }
+  
